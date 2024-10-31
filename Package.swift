9f763caa076c2a6ce893ff0fe8e0f0fe0cfccce7
Alexander Polovinka <alexander@yogaeasy.de>
2024-10-31 23:03:36 +0100

    Add Package.swift and GoogleCastSDK framework

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GoogleCastSDK",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "GoogleCast",
+            targets: ["GoogleCast"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "GoogleCast",
+            path: "./Sources/GoogleCast.xcframework")
+    ]
+)
